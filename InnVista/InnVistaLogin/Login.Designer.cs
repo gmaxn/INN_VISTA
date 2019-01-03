c1ffcf5d044a301e02b73bc8866e20f813dc0a78
Max <maxi.ng@hotmail.com>
2019-01-03 17:24:19 -0300

    -

---
+++ InnVista/InnVistaLogin/Login.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace InnVistaLogin
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_userName = new System.Windows.Forms.Label();
+            this.tbx_userName = new System.Windows.Forms.TextBox();
+            this.tbx_password = new System.Windows.Forms.TextBox();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_nuevoUsuario = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_userName
+            // 
+            this.lbl_userName.AutoSize = true;
+            this.lbl_userName.Location = new System.Drawing.Point(20, 10);
+            this.lbl_userName.Name = "lbl_userName";
+            this.lbl_userName.Size = new System.Drawing.Size(57, 17);
+            this.lbl_userName.TabIndex = 0;
+            this.lbl_userName.Text = "Usuario";
+            // 
+            // tbx_userName
+            // 
+            this.tbx_userName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_userName.ForeColor = System.Drawing.Color.Black;
+            this.tbx_userName.Location = new System.Drawing.Point(20, 37);
+            this.tbx_userName.Name = "tbx_userName";
+            this.tbx_userName.Size = new System.Drawing.Size(200, 22);
+            this.tbx_userName.TabIndex = 1;
+            this.tbx_userName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbx_password
+            // 
+            this.tbx_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_password.ForeColor = System.Drawing.Color.Black;
+            this.tbx_password.Location = new System.Drawing.Point(20, 96);
+            this.tbx_password.Name = "tbx_password";
+            this.tbx_password.PasswordChar = '*';
+            this.tbx_password.Size = new System.Drawing.Size(200, 22);
+            this.tbx_password.TabIndex = 3;
+            this.tbx_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(20, 69);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(81, 17);
+            this.lbl_password.TabIndex = 2;
+            this.lbl_password.Text = "Contraseña";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(20, 135);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(90, 25);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "ingresar";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // btn_nuevoUsuario
+            // 
+            this.btn_nuevoUsuario.Location = new System.Drawing.Point(130, 135);
+            this.btn_nuevoUsuario.Name = "btn_nuevoUsuario";
+            this.btn_nuevoUsuario.Size = new System.Drawing.Size(90, 25);
+            this.btn_nuevoUsuario.TabIndex = 5;
+            this.btn_nuevoUsuario.Text = "nuevo";
+            this.btn_nuevoUsuario.UseVisualStyleBackColor = true;
+            this.btn_nuevoUsuario.Click += new System.EventHandler(this.btn_nuevoUsuario_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 178);
+            this.Controls.Add(this.btn_nuevoUsuario);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tbx_password);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.tbx_userName);
+            this.Controls.Add(this.lbl_userName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inn Vista (Login)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_userName;
+        private System.Windows.Forms.TextBox tbx_userName;
+        private System.Windows.Forms.TextBox tbx_password;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_nuevoUsuario;
+    }
+}
---
+++ InnVista/InnVistaLogin/NuevoUsuario.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace InnVistaLogin
+{
+    partial class NuevoUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_userName = new System.Windows.Forms.Label();
+            this.tbx_userName = new System.Windows.Forms.TextBox();
+            this.tbx_password = new System.Windows.Forms.TextBox();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.btn_nuevoUsuario = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gbx_usuario = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.gbx_datosUsuario = new System.Windows.Forms.GroupBox();
+            this.gbx_usuario.SuspendLayout();
+            this.gbx_datosUsuario.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_userName
+            // 
+            this.lbl_userName.AutoSize = true;
+            this.lbl_userName.Location = new System.Drawing.Point(15, 30);
+            this.lbl_userName.Name = "lbl_userName";
+            this.lbl_userName.Size = new System.Drawing.Size(131, 17);
+            this.lbl_userName.TabIndex = 0;
+            this.lbl_userName.Text = "Nombre de Usuario";
+            // 
+            // tbx_userName
+            // 
+            this.tbx_userName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_userName.ForeColor = System.Drawing.Color.Black;
+            this.tbx_userName.Location = new System.Drawing.Point(15, 56);
+            this.tbx_userName.Name = "tbx_userName";
+            this.tbx_userName.Size = new System.Drawing.Size(200, 22);
+            this.tbx_userName.TabIndex = 1;
+            this.tbx_userName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbx_password
+            // 
+            this.tbx_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_password.ForeColor = System.Drawing.Color.Black;
+            this.tbx_password.Location = new System.Drawing.Point(15, 113);
+            this.tbx_password.Name = "tbx_password";
+            this.tbx_password.PasswordChar = '*';
+            this.tbx_password.Size = new System.Drawing.Size(200, 22);
+            this.tbx_password.TabIndex = 3;
+            this.tbx_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(15, 87);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(42, 17);
+            this.lbl_password.TabIndex = 2;
+            this.lbl_password.Text = "Email";
+            // 
+            // btn_nuevoUsuario
+            // 
+            this.btn_nuevoUsuario.Location = new System.Drawing.Point(125, 495);
+            this.btn_nuevoUsuario.Name = "btn_nuevoUsuario";
+            this.btn_nuevoUsuario.Size = new System.Drawing.Size(90, 25);
+            this.btn_nuevoUsuario.TabIndex = 5;
+            this.btn_nuevoUsuario.Text = "Crear";
+            this.btn_nuevoUsuario.UseVisualStyleBackColor = true;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.Color.Black;
+            this.btn_login.Location = new System.Drawing.Point(15, 495);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(90, 25);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Engañar";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(10, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Apellido";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(10, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombre";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.Black;
+            this.textBox4.Location = new System.Drawing.Point(14, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(200, 22);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "DNI";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "DNI";
+            // 
+            // gbx_usuario
+            // 
+            this.gbx_usuario.Controls.Add(this.label3);
+            this.gbx_usuario.Controls.Add(this.textBox3);
+            this.gbx_usuario.Controls.Add(this.lbl_userName);
+            this.gbx_usuario.Controls.Add(this.tbx_userName);
+            this.gbx_usuario.Controls.Add(this.lbl_password);
+            this.gbx_usuario.Controls.Add(this.tbx_password);
+            this.gbx_usuario.Location = new System.Drawing.Point(425, 85);
+            this.gbx_usuario.Name = "gbx_usuario";
+            this.gbx_usuario.Size = new System.Drawing.Size(230, 205);
+            this.gbx_usuario.TabIndex = 12;
+            this.gbx_usuario.TabStop = false;
+            this.gbx_usuario.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Contraseña";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(15, 170);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(200, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gbx_datosUsuario
+            // 
+            this.gbx_datosUsuario.Controls.Add(this.label2);
+            this.gbx_datosUsuario.Controls.Add(this.textBox2);
+            this.gbx_datosUsuario.Controls.Add(this.textBox4);
+            this.gbx_datosUsuario.Controls.Add(this.label1);
+            this.gbx_datosUsuario.Controls.Add(this.label4);
+            this.gbx_datosUsuario.Controls.Add(this.textBox1);
+            this.gbx_datosUsuario.Location = new System.Drawing.Point(35, 70);
+            this.gbx_datosUsuario.Name = "gbx_datosUsuario";
+            this.gbx_datosUsuario.Size = new System.Drawing.Size(220, 220);
+            this.gbx_datosUsuario.TabIndex = 13;
+            this.gbx_datosUsuario.TabStop = false;
+            this.gbx_datosUsuario.Text = "Datos Usuario";
+            // 
+            // NuevoUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 630);
+            this.Controls.Add(this.gbx_datosUsuario);
+            this.Controls.Add(this.gbx_usuario);
+            this.Controls.Add(this.btn_nuevoUsuario);
+            this.Controls.Add(this.btn_login);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NuevoUsuario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inn Vista (Nuevo Usuario)";
+            this.gbx_usuario.ResumeLayout(false);
+            this.gbx_usuario.PerformLayout();
+            this.gbx_datosUsuario.ResumeLayout(false);
+            this.gbx_datosUsuario.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_userName;
+        private System.Windows.Forms.TextBox tbx_userName;
+        private System.Windows.Forms.TextBox tbx_password;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Button btn_nuevoUsuario;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox gbx_usuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox gbx_datosUsuario;
+    }
+}
